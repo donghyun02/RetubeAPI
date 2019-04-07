69235d61adf841f58339a3e236577f97c2eb9e76
huey <huey@akaon.com>
2019-04-07 17:31:19 +0900

    Add test for Song model

--- playlist/tests/tests_models.py
+++ playlist/tests/tests_models.py
@@ -3,20 +3,16 @@ from django.db import models
 from django.test import TestCase
 
 # Create your tests here.
-from playlist.models import Playlist
+from playlist.models import Playlist, Song
 
 
 class PlaylistTests(TestCase):
 
     def setUp(self):
-        self.username = "username"
-        self.password = "password"
-        self.name = "testname"
-
         user = User.objects.create_user(
-            username=self.username,
-            password=self.password,
-            first_name=self.name
+            username='username',
+            password='password',
+            first_name='testname'
         )
         self.playlist = Playlist.objects.create(name="My playlist", owner=user)
 
@@ -52,7 +48,7 @@ class PlaylistTests(TestCase):
         field = self.playlist._meta.get_field('created')
         self.assertTrue(isinstance(field, models.DateTimeField))
         self.assertEqual(field.verbose_name, 'created')
-        self.assertEqual(field.auto_now_add, True)
+        self.assertTrue(field.auto_now_add)
 
     def test_str(self):
         """
@@ -63,3 +59,87 @@ class PlaylistTests(TestCase):
         name = self.playlist.name
         self.assertEqual(title, '{} - {}'.format(owner_first_name, name))
 
+class SongTests(TestCase):
+
+    def setUp(self):
+        user = User.objects.create_user(
+            username="username",
+            password="password",
+            first_name="testname"
+        )
+        playlist = Playlist.objects.create(
+            name="My play list",
+            owner=user
+        )
+        self.song = Song.objects.create(
+            name="Song",
+            video_id="test_video_id",
+            thumbnail="https://thumbnail.url",
+            playlist=playlist
+        )
+
+    def test_verbose_name(self):
+        """
+        모델 이름 테스트
+        """
+        verbose_name = self.song._meta.verbose_name
+        self.assertEqual(verbose_name, 'song')
+
+    def test_name_field(self):
+        """
+        name 필드 테스트
+        """
+        field = self.song._meta.get_field('name')
+        self.assertTrue(field, models.CharField)
+        self.assertEqual(field.verbose_name, 'name')
+        self.assertEqual(field.max_length, 256)
+
+    def test_video_id_field(self):
+        """
+        video_id 필드 테스트
+        """
+        field = self.song._meta.get_field('video_id')
+        self.assertTrue(field, models.CharField)
+        self.assertEqual(field.verbose_name, 'video id')
+        self.assertEqual(field.max_length, 32)
+        self.assertTrue(field._unique)
+
+    def test_thumbnail_field(self):
+        """
+        thumbnail 필드 테스트
+        """
+        field = self.song._meta.get_field('thumbnail')
+        self.assertTrue(field, models.TextField)
+        self.assertEqual(field.verbose_name, 'thumbnail')
+
+    def test_playlist_field(self):
+        """
+        playlist 필드 테스트
+        """
+        field = self.song._meta.get_field('playlist')
+        self.assertTrue(field, models.ForeignKey)
+        self.assertEqual(field.verbose_name, 'playlist')
+        self.assertEqual(field.related_model, Playlist)
+
+    def test_created_field(self):
+        """
+        created 필드 테스트
+        """
+        field = self.song._meta.get_field('created')
+        self.assertTrue(field, models.DateTimeField)
+        self.assertEqual(field.verbose_name, 'created')
+        self.assertTrue(field.auto_now_add)
+
+    def test_str_(self):
+        """
+        __str__ 매직 메서드 테스트
+        """
+        self.assertEqual(str(self.song), self.song.name)
+
+    def test_indexed_fields(self):
+        """
+        인덱싱 필드 테스트
+        """
+        video_id_index = models.Index(fields=['video_id']).fields
+        indexes = (index.fields for index in self.song._meta.indexes)
+        self.assertTrue(video_id_index in indexes)
